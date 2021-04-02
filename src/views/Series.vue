a070ef6fd1b6e3654b83ae004a06ab0051127ebf
Dyoma3 <dinko.f.yoma@gmail.com>
2021-04-02 16:33:59 -0300

    add seasons

--- src/views/Series.vue
+++ src/views/Series.vue
@@ -1,10 +1,96 @@
 <template>
-    <div>
-
-    </div>
+    <v-container >
+        <v-overlay v-if="loading" fixed>
+            <v-progress-circular
+                :size="70"
+                :width="7"
+                color="#e50914"
+                indeterminate
+            />
+        </v-overlay>
+        <v-row
+            class="justify-center mt-7"
+            style="color:white;font-size:30px"
+        >
+            {{ this.series }}
+        </v-row>
+        <v-row justify="center">
+            <v-expansion-panels class="mt-10"
+                style="max-width:500px"
+            >
+                <v-expansion-panel
+                    v-for="(season, i) in seasons"
+                    :key="i"
+                    style="background-color:#0e0e0e;max-width:500px;"
+                >
+                    <v-expansion-panel-header style="color:white">
+                        <v-row justify="center" style="font-size:19px">
+                            Temporada {{ season }}
+                        </v-row>
+                        <template v-slot:actions>
+                            <v-icon color="white">
+                                mdi-menu-down
+                            </v-icon>
+                        </template>
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <div style="color:white;font-size:25px">
+                            Episodios
+                        </div>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </v-row>
+    </v-container>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data: () => ({
+        loading: true,
+        seasons: [],
+        seasonEpisodes: [],
+    }),
+    computed: {
+        series() {
+            return this.$route.name
+        },
+    },
+    created() {
+        const seriesParameter = this.series === 'Breaking Bad'
+            ? 'Breaking+Bad'
+            : 'Better+Call+Saul';
+        axios({
+            method: 'get',
+            url: `https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=${seriesParameter}`,
+        })
+        .then((result) => {
+            result.data.forEach(item => {
+                if (!this.seasons.includes(item.season)) {
+                    this.seasons.push(item.season);
+                    this.seasonEpisodes[item.season] = [{
+                        title: item.episode_id,
+                        id: item.id,
+                        number: item.episode
+                    }];
+                } else {
+                    this.seasonEpisodes[item.season].push({
+                        id: item.episode_id,
+                        title: item.title,
+                        number: item.episode,
+                    })
+                }
+            });
+            console.log(this.seasons);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            this.loading = false;
+        })
+    },
+
 };
 </script>
