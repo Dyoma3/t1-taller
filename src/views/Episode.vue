b0621f336e42990649e37dbebe8e13338df9d80c
Dyoma3 <dinko.f.yoma@gmail.com>
2021-04-02 20:37:42 -0300

    add character info

--- src/views/Episode.vue
+++ src/views/Episode.vue
@@ -10,8 +10,7 @@
         </v-overlay>
         <v-row
             v-if="episodeData"
-            class="justify-center my-10"
-            style="color:white;font-size:30px"
+            class="justify-center my-10 main-text"
         >
             {{ episodeData.title }}
         </v-row>
@@ -19,58 +18,197 @@
             v-if="episodeData"
             style="background-color:#0e0e0e"
         >
-            <v-row class="pa-6" style="width:800px">
-                <v-col
-                    class="pr-6"
-                    cols="6"
-                >   
-                    <v-row>
-                        <div class="subtitle mr-2">
-                            Temporada:
-                        </div>
-                        <div class="data-text">
-                            {{ episodeData.season }}
-                        </div>
-                    </v-row>
-                    <v-row>
-                        <div class="subtitle mr-2">
-                            Episodio:
-                        </div>
-                        <div class="data-text">
-                            {{ episodeData.episode }}
-                        </div>
-                    </v-row>
-                    <v-row>
-                        <div class="subtitle mr-2">
-                            Fecha:
-                        </div>
-                        <div class="data-text">
-                            {{ date }}
-                        </div>
-                    </v-row>
+            <v-container class="pa-6" style="width:500px"> 
+                <v-row>
+                    <div class="subtitle mr-2">
+                        Temporada:
+                    </div>
+                    <div class="data-text">
+                        {{ episodeData.season }}
+                    </div>
+                </v-row>
+                <v-row>
+                    <div class="subtitle mr-2">
+                        Episodio:
+                    </div>
+                    <div class="data-text">
+                        {{ episodeData.episode }}
+                    </div>
+                </v-row>
+                <v-row>
+                    <div class="subtitle mr-2">
+                        Fecha:
+                    </div>
+                    <div class="data-text">
+                        {{ date }}
+                    </div>
+                </v-row>
 
-                </v-col>
-                <v-col
-                    class="pr-6"
-                    cols="6"
-                >
-                    <v-row>
-                        <div class="subtitle mr-2">
-                            Personajes:
-                        </div>
-                        <div 
-                            v-for="(character, i) in episodeData.characters"
+                <v-row>
+                    <v-col cols="6" class="subtitle pa-0 mr-2">
+                        Personajes:
+                    </v-col>
+                    <v-col cols="6" class="d-flex flex-column align-center"
+                        v-for="(character, i) in episodeData.characters"
                             :key="i"
-                            class="data-text"
+                    >
+                        <v-hover 
+                            v-slot="{ hover }"    
                         >
-                            {{ character }}
-                        </div>
-                    </v-row>
+                            <a
+                                @click="openCharacter(character)"
+                                :style="{
+                                    textTransform: 'none',
+                                    textAlign: 'center',
+                                    color: hover ? '#2C384A' : '#039BE5',
+                                    fontSize: '22px',
+                                }"
+                            >
+                                {{ character }}
+                            </a>
+                        </v-hover>
+                    </v-col>
+                </v-row>
 
-                </v-col>
-            </v-row>
+            </v-container>
 
         </v-card>
+        <v-dialog v-model="characterVisible"
+            transition="dialog-bottom-transition"
+            width="550"
+        >
+            <v-container
+            class="pt-0 px-0 ma-0"
+            style="background-color:#2b2727;overflow:hidden">
+                <div
+                class="d-flex flex-column align-center"
+                style="background-color:#0e0e0e;position:relative">
+                    <v-img
+                        v-if="selectedCharacterData"
+                        contain
+                        height="200px"
+                        :src="selectedCharacterData.img"
+                    >
+
+                    </v-img>
+                    <div
+                        class="main-text my-3"
+                    >
+                        {{ selectedCharacter }}
+                    </div>
+
+                    <v-icon color="white"
+                        @click="characterVisible = false"
+                        style="position:absolute; right:8px;top:10px"
+                    >
+                        mdi-close
+                    </v-icon>
+                </div>
+                <v-row
+                    v-if="loadingCharacter"
+                    style="height: 300px;position:relative"
+                >
+                    <v-overlay absolute>
+                        <v-progress-circular
+                            :size="70"
+                            :width="7"
+                            color="#e50914"
+                            indeterminate
+                        />
+                    </v-overlay>
+                </v-row>
+                <div v-if="selectedCharacterData && !loadingCharacter" class="d-flex flex-column pa-5">
+                    <v-row>
+                        <div class="subtitle mr-2">
+                            Nickname:
+                        </div>
+                        <div class="data-text">
+                            {{ selectedCharacterData.nickname }}
+                        </div>
+                    </v-row>
+                    <v-row>
+                        <div class="subtitle mr-2">
+                            Roles:
+                        </div>
+                        <div class="data-text">
+                            {{ characterOccupations }}
+                        </div>
+                    </v-row>
+                    <v-row>
+                        <div class="subtitle mr-2">
+                            Actor/Actriz:
+                        </div>
+                        <div class="data-text">
+                            {{ selectedCharacterData.portrayed }}
+                        </div>
+                    </v-row>
+                    <v-row>
+                        <div class="subtitle mr-2">
+                            Estado:
+                        </div>
+                        <div class="data-text">
+                            {{ selectedCharacterData.status }}
+                        </div>
+                    </v-row>
+                    <v-row>
+                        <div cols="6" class="subtitle mr-2">
+                            Temporadas Breaking Bad:
+                        </div>
+
+                        <v-hover 
+                            v-for="(season, i) in selectedCharacterData.appearance"
+                            :key="i"
+                            v-slot="{ hover }"    
+                        >
+                            <a
+                                class="mr-2"
+                                @click="openCharacter(character)"
+                                :style="{
+                                    textTransform: 'none',
+                                    textAlign: 'center',
+                                    color: hover ? '#2C384A' : '#039BE5',
+                                    fontSize: '22px',
+                                }"
+                            >
+                                {{ 
+                                    i === selectedCharacterData.appearance.length - 1
+                                    ? season
+                                    :`${season},`
+                                }}
+                            </a>
+                        </v-hover>
+                    </v-row>
+                    <v-row>
+                        <div cols="6" class="subtitle mr-2">
+                            Temporadas Better Call Saul:
+                        </div>
+
+                        <v-hover 
+                            v-for="(season, i) in selectedCharacterData.better_call_saul_appearance"
+                            :key="i"
+                            v-slot="{ hover }"    
+                        >
+                            <a
+                                class="mr-2"
+                                @click="openCharacter(character)"
+                                :style="{
+                                    textTransform: 'none',
+                                    textAlign: 'center',
+                                    color: hover ? '#2C384A' : '#039BE5',
+                                    fontSize: '22px',
+                                }"
+                            >
+                                {{ 
+                                    i === selectedCharacterData.appearance.length - 1
+                                    ? season
+                                    :`${season},`
+                                }}
+                            </a>
+                        </v-hover>
+                    </v-row>
+                </div>
+            </v-container>
+        </v-dialog>
     </v-container>
 </template>
 
@@ -81,6 +219,7 @@ export default {
     data: () => ({
         episodeData: null,
         loading: true,
+        loadingCharacter: false,
         monthDic: {
             '01': 'enero',
             '02': 'febrero',
@@ -94,13 +233,51 @@ export default {
             '10': 'octubre',
             '11': 'noviembre',
             '12': 'diciembre',
-        }
+        },
+        selectedCharacter: null,
+        characterVisible: false,
+        selectedCharacterData: null,
     }),
     computed: {
         date() {
             const dateString = this.episodeData.air_date.split('T')[0];
             const dateArray = dateString.split('-');
             return `${dateArray[2]} de ${this.monthDic[dateArray[1]]}, ${dateArray[0]}`;
+        },
+        birthday() {
+            if (!this.selectedCharacterData) {
+                return '';
+            } else {
+                const birthdayArray = this.selectedCharacterData.birthday.split('-');
+                return `${birthdayArray[0]} de ${this.monthDic[birthdayArray[1]]}, ${birthdayArray[2]}`;
+            }
+        },
+        characterOccupations() {
+            if (!this.selectedCharacterData) {
+                return '';
+            } else {
+                return this.selectedCharacterData.occupation.join(', ');
+            }
+        },
+    },
+    methods: {
+        openCharacter(character) {
+            this.selectedCharacter = character;
+            this.characterVisible = true;
+            axios({
+                method: 'get',
+                url: `https://tarea-1-breaking-bad.herokuapp.com/api/characters/?name=${character}`,
+            })
+            .then((result) => {
+                this.selectedCharacterData = result.data[0];
+                console.log(result.data[0]);
+            })
+            .catch(() => {
+                this.characterVisible = false;
+            })
+            .finally(() => {
+                this.loadingCharacter = false;
+            });
         },
     },
 
@@ -111,7 +288,6 @@ export default {
         })
         .then((result) => {
             this.episodeData = result.data[0];
-            console.log(this.episodeData);
         })
         .finally(() => {
             this.loading = false;
@@ -120,6 +296,10 @@ export default {
 };
 </script>
 <style scoped>
+.main-text {
+    color:white;
+    font-size:30px;
+}
 .subtitle {
     color:grey;
     font-size: 22px;
